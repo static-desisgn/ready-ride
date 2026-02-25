810883ad5fd1f7cdc5f4b537f669355e449280f8
fahmid munshi <fahmidmunshi2004@gmail.com>
2026-02-25 10:21:17 +0600

    issu fix

--- js/Swiper.js
+++ js/Swiper.js
@@ -1,54 +1,31 @@
 // Swiper 1
 var swiper1 = new Swiper(".rider-slide", {
+    slidesPerView: "auto",
     spaceBetween: 24,
     loop: true,
-    speed: 5000,
+    speed: 6000,
     freeMode: true,
     freeModeMomentum: false,
+    allowTouchMove: true,
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
-        reverseDirection: false,
     },
-    allowTouchMove: true,
-
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        576: {
-            slidesPerView: 2,
-        },
-        992: {
-            slidesPerView: 3,
-        }
-    }
 });
 
 
 // Swiper 2
 var swiper2 = new Swiper(".rider-slide-2", {
+    slidesPerView: "auto",
     spaceBetween: 24,
     loop: true,
-    speed: 5000,
+    speed: 6000,
     freeMode: true,
     freeModeMomentum: false,
+    allowTouchMove: true,
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
         reverseDirection: true,
     },
-    allowTouchMove: true,
-
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        576: {
-            slidesPerView: 2,
-        },
-        992: {
-            slidesPerView: 3,
-        }
-    }
 });
